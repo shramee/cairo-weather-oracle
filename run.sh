f6a4557913ddaf8359f00490dee8eea7389b49c2
Shramee Srivastav <shramee.srivastav@gmail.com>
2022-09-13 00:52:15 +0800

    Updating environment files

--- run.sh
+++ run.sh
@@ -1,32 +1,29 @@
 #!/bin/sh
-
 MAX_FEE=1600000000000
-if [ $1 ];
-	then _SCRIPT=$1;
-	else _SCRIPT=$RUN_SCRIPT;
-fi
 
-HORIZONTAL_RULE="----------------------------------------------------------------------"
+# This file runs scripts, this file shouldn't be edited directly.
+# You can add your custom scripts in scripts.sh
+
+if [ $1 ];
+	then RUN_SCRIPT=$1;
+fi
+LOG_FILE="./logs/script-$RUN_SCRIPT.log"
+HR="----------------------------------------------------------------------"
 DATE=$(date +'%Y-%m-%dT%H:%M:%S%Z')
-echo ""
-echo -e "$DATE - Running script $_SCRIPT"
-echo -e $HORIZONTAL_RULE
+
+printf '\n%s\n%s\n' "$DATE - Running script $RUN_SCRIPT" $HR | tee -a $LOG_FILE
 
 deploy_compiled_contract() {
 	if [ $1 ]
 		then
-		echo "Declaring class for $1..."
+		echo "Declaring class..."
 
 		DECLARED=$(starknet declare --contract $1 --max_fee $MAX_FEE)
 		echo "$DECLARED"
 
 		CLASS_HASH=`echo "$DECLARED" | grep class | sed "s/Contract class hash: //"`
 
-		# Sleep for 2 seconds
-		sleep 2
-
 		echo "Deploying contract for class $CLASS_HASH..."
-
 		DEPLOYED=`starknet deploy --class_hash $CLASS_HASH --max_fee $MAX_FEE`
 
 		echo "$DEPLOYED"
@@ -37,58 +34,6 @@ deploy_compiled_contract() {
 
 # You can call your scripts using `RUN_SCRIPT=my_script docker compose up`
 
-case $_SCRIPT in
+source ./scripts.sh | tee -a $LOG_FILE
 
-# Add your custom scripts here
-	
-	"my") # This line begins script `my_script`
-		find / -type d -name 'starkware'
-		# ls /tmp/cairo_libs
-	;; # Don't forget double semicolon `;;` to end script
-
-
-# Here are some example scripts, edit away as you please.
-
-	"test")
-	
-		# Compile src/test.cairo to build/test_compiled.json
-		cairo-compile ./src/test.cairo --output ./build/test_compiled.json
-
-		# Run build/test_compiled.json
-		cairo-run \
-		--program=./build/test_compiled.json --print_output \
-		--print_info --relocate_prints --layout=small
-	;;
-
-	"contract")
-		starknet-compile ./src/my-contract.cairo \
-		--output ./build/my-contract_compiled.json \
-		--abi ./build/my-contract_abi.json
-	;;
-
-	"contract_deploy")
-		# deploy_compiled_contract
-		deploy_compiled_contract ./build/my-contract_compiled.json
-	;;
-# Stop editing, these are default scripts.
-
-	"deploy_account")
-		starknet deploy_account
-	;;
-
-	"install")
-		# Add code here for setup/configuration (like installing packages)
-		# This will be called when building the image
-		# Can be called after changes with,
-		# RUN_SCRIPT=install docker compose up
-		# 
-		echo "Installation done!";
-	;;
-
-	*)
-		echo "Script '$_SCRIPT' is not defined.";
-esac
-
-echo -e $HORIZONTAL_RULE
-echo -e "Finished running script $_SCRIPT"
-echo ""
+printf '%s\n%s\n' $HR "Finished running script $RUN_SCRIPT" | tee -a $LOG_FILE
---
+++ scripts.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+case $RUN_SCRIPT in
+# Add your custom scripts here
+	
+	"my_script") # This line begins script `my_script`
+		# Shell commands here
+		echo "This script doesn't do very much at the moment."
+	;; # Don't forget double semicolon `;;` to end script
+
+
+# Here are some example scripts, edit away as you please.
+
+	"test")
+		# Compile src/test.cairo to build/test.json
+		cairo-compile ./src/test.cairo --output ./build/test.json
+
+		# Run build/test.json
+		cairo-run \
+		--program=./build/test.json --print_output \
+		--print_info --relocate_prints --layout=small
+	;;
+
+	"contract")
+
+		starknet-compile ./src/my-contract.cairo \
+		--output ./build/my-contract.json \
+		--abi ./build/my-contract_abi.json
+
+		# deploy_compiled_contract is a custom function to declare a class from a file
+		# and then deploy a contract based on the class hash.
+		deploy_compiled_contract ./build/my-contract.json
+	;;
+
+	"deploy_account")
+		starknet deploy_account
+	;;
+
+	"install")
+		# Add code here for setup/configuration (like installing packages)
+		# This will be called when building the image
+		# Can be called after changes with,
+		# RUN_SCRIPT=install docker compose up
+		# 
+		echo "Installation done!";
+	;;
+
+	*)
+		echo "Script '$RUN_SCRIPT' is not defined.";
+esac
